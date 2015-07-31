f7e58e122a0c611179169a89aa5ce03768def030
unknown <Greg.Jackson@gjackson2.eprize.local>
2015-07-31 13:59:41 -0400

    main.js - adding input functionality

--- js/main.js
+++ js/main.js
@@ -28,11 +28,60 @@ var GameState = {
 		// custom properties
 		this.pet.customParams = {health: 100, fun: 100};
 
+		//Draggable Pet
+	    this.pet.inputEnabled = true;
+	    this.pet.input.enableDrag();
+
 		this.apple = this.game.add.sprite(72, 570, 'apple');
+		this.apple.anchor.setTo(0.5);
+		this.apple.inputEnabled = true;
+		this.apple.customParams = {health: 20};
+		this.apple.events.onInputDown.add(this.pickItem, this);
+
+
 		this.candy = this.game.add.sprite(144, 570, 'candy');
+		this.candy.anchor.setTo(0.5);
+		this.candy.inputEnabled = true;
+		this.candy.customParams = {health: -20, fun: 10};
+		this.candy.events.onInputDown.add(this.pickItem, this);
+
 		this.toy = this.game.add.sprite(220, 570, 'toy');
+		this.toy.anchor.setTo(0.5);
+		this.toy.inputEnabled = true;
+		this.toy.customParams = {fun: 20};
+		this.toy.events.onInputDown.add(this.pickItem, this);
+
 		this.rotate = this.game.add.sprite(288, 570, 'rotate');
+		this.rotate.anchor.setTo(0.5);
+		this.rotate.inputEnabled = true;
+		this.rotate.events.onInputDown.add(this.rotatePet, this);
+
+		this.buttons = [this.apple, this.candy, this.toy, this.rotate];
+
+		//current selected item
+		this.selectedItem = null;
+		this.uiBlocked = false; 
+
 	},
+
+	pickItem: function(sprite, event) {
+		if(!this.uiBlocked) {
+			console.log("pick item");
+
+			this.clearSelection();
+			sprite.alpha = 0.4;
+
+			this.selectedItem = sprite;
+		}
+	},
+
+	rotatePet: function(sprite, event) {
+		console.log("Rotate Pet");
+	},
+	
+	clearSelection: function() {
+
+	}
 
 
 };
